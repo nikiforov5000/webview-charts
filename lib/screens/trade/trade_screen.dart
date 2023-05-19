c2712fc17f1af85dfe45d4da772c239379d79f56
boris <nikiforov5000@gmail.com>
2023-05-19 08:19:40 +0300

    webview works

--- lib/screens/trade/trade_screen.dart
+++ lib/screens/trade/trade_screen.dart
@@ -1,9 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'package:webview_flutter_android/webview_flutter_android.dart';
+
 import 'package:test_job_slavit_ou/screens/currency_pair/currency_pair_screen.dart';
 
-class TradeScreen extends StatelessWidget {
+class TradeScreen extends StatefulWidget {
   static const String id = 'TradeScreen';
-  const TradeScreen({Key? key}) : super(key: key);
+
+  TradeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<TradeScreen> createState() => _TradeScreenState();
+}
+
+class _TradeScreenState extends State<TradeScreen> {
+  WebViewController _controller = WebViewController();
+
+  @override
+  void initState() {
+
+    _controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setBackgroundColor(const Color(0x00000000))
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onProgress: (int progress) {
+            // Update loading bar.
+          },
+          onPageStarted: (String url) {},
+          onPageFinished: (String url) {},
+          onWebResourceError: (WebResourceError error) {},
+          onNavigationRequest: (NavigationRequest request) {
+            if (request.url.startsWith('https://www.youtube.com/')) {
+              return NavigationDecision.prevent;
+            }
+            return NavigationDecision.navigate;
+          },
+        ),
+      )
+      ..loadRequest(Uri.parse('https://flutter.dev'));
+
+    super.initState();
+  }
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -19,6 +59,13 @@ class TradeScreen extends StatelessWidget {
               },
               child: Text('go to CurrencyPair'),
             ),
+            Container(
+              child: Expanded(
+                child: WebViewWidget(
+                  controller: _controller,
+                ),
+              ),
+            )
           ],
         ),
       ),

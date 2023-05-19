253995c0104293dd16120e62183727ca0a34ead2
boris <nikiforov5000@gmail.com>
2023-05-19 18:11:39 +0300

    opens chart in webview

--- lib/screens/trade/trade_screen.dart
+++ lib/screens/trade/trade_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
-import 'package:webview_flutter_android/webview_flutter_android.dart';
-
 import 'package:test_job_slavit_ou/screens/currency_pair/currency_pair_screen.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+// Password DsAut9LmWUGZCC4
 
 class TradeScreen extends StatefulWidget {
   static const String id = 'TradeScreen';
@@ -17,36 +17,8 @@ class _TradeScreenState extends State<TradeScreen> {
   WebViewController _controller = WebViewController();
 
   @override
-  void initState() {
-
-    _controller = WebViewController()
-      ..setJavaScriptMode(JavaScriptMode.unrestricted)
-      ..setBackgroundColor(const Color(0x00000000))
-      ..setNavigationDelegate(
-        NavigationDelegate(
-          onProgress: (int progress) {
-            // Update loading bar.
-          },
-          onPageStarted: (String url) {},
-          onPageFinished: (String url) {},
-          onWebResourceError: (WebResourceError error) {},
-          onNavigationRequest: (NavigationRequest request) {
-            if (request.url.startsWith('https://www.youtube.com/')) {
-              return NavigationDecision.prevent;
-            }
-            return NavigationDecision.navigate;
-          },
-        ),
-      )
-      ..loadRequest(Uri.parse('https://flutter.dev'));
-
-    super.initState();
-  }
-
-
-
-  @override
   Widget build(BuildContext context) {
+    _controller..loadFlutterAsset('assets/html/tradeingview.html')..setJavaScriptMode(JavaScriptMode.unrestricted);
     return Scaffold(
       body: Center(
         child: Column(

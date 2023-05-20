8d4f82e4065acb2212e83014535a900a5054c854
boris <nikiforov5000@gmail.com>
2023-05-20 11:57:53 +0300

    charts html code moved from asset file to a function

--- lib/screens/trade/trade_screen.dart
+++ lib/screens/trade/trade_screen.dart
@@ -18,7 +18,9 @@ class _TradeScreenState extends State<TradeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    _controller..loadFlutterAsset('assets/html/tradeingview.html')..setJavaScriptMode(JavaScriptMode.unrestricted);
+    _controller..loadHtmlString(html())
+      // ..loadFlutterAsset('assets/html/tradeingview.html')
+      ..setJavaScriptMode(JavaScriptMode.unrestricted);
     return Scaffold(
       body: Center(
         child: Column(
@@ -43,4 +45,35 @@ class _TradeScreenState extends State<TradeScreen> {
       ),
     );
   }
+}
+
+String html() {
+  String html =
+    '<div class="tradingview-widget-container">'
+        '<div id="tradingview_91dde"></div>'
+  '<div class="tradingview-widget-copyright"><a href="https://www.tradingview.com/"'
+  'rel="noopener nofollow" target="_blank"><span class="blue-text">Track all markets on TradingView</span></a></div>'
+  '<script type="text/javascript" src="https://s3.tradingview.com/tv.js"></script>'
+  '<script type="text/javascript">'
+  'new TradingView.widget('
+  '{'
+  '"autosize": true,'
+  '"symbol": "FX:EURUSD",'
+  '"interval": "D",'
+  '"timezone": "Etc/UTC",'
+  '"theme": "light",'
+  '"style": "1",'
+  '"locale": "en",'
+  '"toolbar_bg": "#f1f3f6",'
+  '"enable_publishing": false,'
+  '"hide_top_toolbar": true,'
+  '"hide_legend": true,'
+  '"save_image": false,'
+  '"container_id": "tradingview_b368b"'
+  '}'
+  ');'
+  '</script>'
+  '</div>';
+
+  return html;
 }

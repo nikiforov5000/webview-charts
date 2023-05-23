492382b90bad9bc9853b9d0e6e43fb2c9eb91065
boris <nikiforov5000@gmail.com>
2023-05-23 14:56:04 +0300

    notification works

--- lib/screens/preloader/widgets/progress_bar.dart
+++ lib/screens/preloader/widgets/progress_bar.dart
@@ -1,8 +1,26 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:test_job_slavit_ou/screens/home/home_screen.dart';
 
 class ProgressBar extends StatelessWidget {
-  const ProgressBar({Key? key}) : super(key: key);
+  ProgressBar({Key? key}) : super(key: key);
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+
+  requestPushNotifications() async {
+    NotificationSettings settings = await messaging.requestPermission(
+      alert: true,
+      announcement: false,
+      badge: true,
+      carPlay: false,
+      criticalAlert: false,
+      provisional: false,
+      sound: true,
+    );
+    print('User granted permission: ${settings.authorizationStatus}');
+
+    return settings.authorizationStatus;
+
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,6 +31,7 @@ class ProgressBar extends StatelessWidget {
       tween: Tween(begin: 0, end: 1),
       duration: const Duration(seconds: 3),
       onEnd: () {
+        requestPushNotifications();
         Future.delayed(Duration.zero, () {
           Navigator.pushNamed(context, HomeScreen.id);
         });
